a352586db0ef8d3d0cf5d0c46a3775a2962d315b
Jordi <55429631+jorbush@users.noreply.github.com>
2024-08-30 18:16:17 +0200

    Add Nunito font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './src/**/*.{vue,ts}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Nunito', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
