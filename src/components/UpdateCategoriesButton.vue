2e9f7b2f163cb2b92ab9a78038856f29bff05341
Jordi <55429631+jorbush@users.noreply.github.com>
2024-08-31 19:37:43 +0200

    Update update categories icon

--- src/components/UpdateCategoriesButton.vue
+++ src/components/UpdateCategoriesButton.vue
@@ -7,17 +7,20 @@
       <span class="hidden sm:inline">Update Categories</span>
       <svg
         class="w-6 h-6 sm:hidden"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
+        aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        viewBox="0 0 24 24"
       >
         <path
+          stroke="currentColor"
           stroke-linecap="round"
           stroke-linejoin="round"
           stroke-width="2"
-          d="M12 4v16m8-8H4"
-        ></path>
+          d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"
+        />
       </svg>
     </button>
     <input
